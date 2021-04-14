b0b4a275f819943d7bc7731580a0185cff26c537
Stu Thompson <stuart@stuartthompson.net>
2021-04-13 18:07:50 -0700

    SyncFiles: Exclude secure

--- syncfiles.sh
+++ syncfiles.sh
@@ -2,9 +2,9 @@ if [ X"$1" = X"-e" ]
 then
   # Sync files
   echo "Syncing files with home directory."
-  rsync -av --exclude=.git --exclude=README.md --exclude=LICENSE.md --exclude=syncfiles.sh ./ ~ 
+  rsync -av --exclude=.git --exclude=.config/secure --exclude=README.md --exclude=LICENSE.md --exclude=syncfiles.sh ./ ~ 
 else
   # Execute flag not found, do a dry run
   echo "Performing a dry run. Add -e to execute the sync."
-  rsync -anv --exclude=.git --exclude=README.md --exclude=LICENSE.md --exclude=syncfiles.sh . ~
+  rsync -anv --exclude=.git --exclude=.config/secure --exclude=README.md --exclude=LICENSE.md --exclude=syncfiles.sh . ~
 fi
